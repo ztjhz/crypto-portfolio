f0dd9ac521e9c08ac70f994e73cb541a2b531f62
ztjhz <59118459+ztjhz@users.noreply.github.com>
2021-07-15 10:23:37 +0800

    updated yahoo_usd_sgd function

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def get_yahoo_finance_USD_SGD_rate():
     except requests.exceptions.RequestException as e:
         print(e)
     
-    USDSGD = float(response.html.xpath("//span[@data-reactid='32']")[0].text)
+    USDSGD = float(response.html.xpath("//div[@data-reactid='30']/span[@data-reactid='31']")[0].text)
     return(USDSGD)
 
 def get_mas_USD_SGD_rate():
